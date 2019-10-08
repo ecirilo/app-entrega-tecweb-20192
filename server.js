03f056291dc16ec44f4b858e207b1324259da0b0
Elde Cirilo <elder@Eldes-MacBook-Air.local>
2019-10-08 11:23:50 -0300

    Modificações parcial - Aula 08/10/2019

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456789',
+    database: 'entrega'
+});
+
 app.use(express.json());
 
 app.get('/entregador/:id', (req, resp) => {
-    console.log('Chamou -> /entregador/:id');
-    resp.status(200).end();
+    let id_entregador = req.params.id;
+
+    connection.query("SELECT * FROM entregadores WHERE identregador = ?",
+    [id_entregador],
+    (err, result) => {
+        
+        if (err) {
+            console.log(err);
+            resp.status(500).end();
+        } else {
+            
+            resp.json(result);            
+        }
+    });    
 });
 
 app.post('/entregador', (req, resp) => {
-    console.log('Chamou -> /entregador');
-    resp.status(200).end();
+    let entregador = req.body;
+
+    if (entregador == null) {
+        resp.status(204).end();
+    } else {
+        connection.query('INSERT INTO entregadores SET ?',
+        [entregador], 
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                resp.status(500).end();
+            } else {
+                resp.status(200);
+                resp.send(result);
+            }
+        });
+    }    
 });
 
 app.put('/entregador/:id', (req, resp) => {
@@ -99,5 +134,8 @@ app.put('/envio/:id/avaliar', (req, resp) => {
 });
 
 app.listen('3000', () => {
-    console.log('Backend escutando na porta 3000 ...')
+    connection.connect((err) => {
+        if (err) return console.log(err);
+        console.log('Backend conectado e escutando na porta 3000 ...')
+    });    
 });
